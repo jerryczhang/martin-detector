aecf7ae4d6e59d4a51b46265480a076fbbfe4063
jerryczhang <jerrychzhang@gmail.com>
2020-12-17 09:44:04 -0800

    Modify face script for pulling multiple faces from original images

--- facecrop.py
+++ facecrop.py
@@ -3,18 +3,20 @@ from PIL import Image
 from glob import glob
 import os
 
-def get_face(pil_image, cascade):
-    return pil_image
+def pull_faces(pil_image, cascade):
+    return [pil_image]
 
-def gen_faces(srcdir, dest, cascade):
+def get_face_images(srcdir, dest, cascade):
     if not os.path.exists(dest):
         os.makedirs(dest)
-    images = glob(f'{srcdir}/*.*', recursive=True)
+    images = glob(f'{srcdir}/*.*')
     for image in images:
         pil_image = Image.open(image)
-        face_image = get_face(pil_image)
-        face_image.save(f'{dest}/name)')
+        face_images = pull_faces(pil_image, cascade)
+        for face_image in face_images:
+            name = image.split(os.sep)[-1]
+            face_image.save(f'{dest}/{name}')
 
 cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-gen_faces('images/raw', 'images/cropped', cascade)
+get_face_images('images/raw/alex', 'images/cropped', cascade)
 
